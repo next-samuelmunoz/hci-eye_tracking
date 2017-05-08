ae714439eb98232e1b584712c7eded7b7537e79c
beeva-enriqueotero <enrique.otero@beeva.com>
2017-05-08 18:25:10 +0200

    Mirror transformation

--- eye_tracking/utils/data_augmentation.py
+++ eye_tracking/utils/data_augmentation.py
@@ -2,6 +2,18 @@
 """Perform data augmentation on images
 """
 
+import pygame
+from pygame.locals import *
+from data import Data
+import skimage
+from skimage import io
+from skimage import transform
+import numpy as np
+import os
+
+PATH_DATA = '../data/'
+PATH_DATA_RAW = '../data/raw/'
+PATH_DATA_AUGMENTED = '../data/augmented/'
 
 def data_augmentation(img, transformations=[]):
     """Iterate over transformations and return the transformed image.
@@ -32,8 +44,8 @@ def data_augmentation(img, transformations=[]):
 def mirror(img):
     """Vertical symmetry
     """
-    yield img
-    pass
+    img = np.fliplr(img)
+    return img
 
 
 def noise(img):
@@ -48,3 +60,27 @@ def luminance(img):
     """
     yield img
     pass
+
+
+def loop(data_list):
+    for i_data in range(len(data_list)):
+        img = io.imread(data_list[i_data]['img_path'])
+        print(data_list[i_data])
+
+        mypath = data_list[i_data]['img_path'].replace(PATH_DATA_RAW, PATH_DATA_AUGMENTED)
+        dest_dir = os.path.dirname(mypath)
+
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+
+        img = mirror(img)
+        io.imsave(mypath, img)
+
+
+data = Data(PATH_DATA_RAW)
+data_list = list(data.iterate())
+if data_list:
+    print "NUMBER OF SAMPLES: {}".format(len(data_list))
+    loop(data_list)
+else:
+    print "No data"
